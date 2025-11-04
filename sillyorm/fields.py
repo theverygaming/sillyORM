08e2be9c3d10a1270ff55a8edf17488a8a075424
theverygaming <theverygaming@users.noreply.github.com>
2025-11-04 09:42:49 +0100

    fix linter

--- sillyorm/fields.py
+++ sillyorm/fields.py
@@ -870,7 +870,10 @@ class Many2many(Field):
             sqlalchemy.UniqueConstraint(
                 self._join_table_self_name,
                 self._join_table_foreign_name,
-                name=f"{table_name_sanitized}_{self._join_table_self_name}_{self._join_table_foreign_name}_unique",
+                name=(
+                    f"{table_name_sanitized}_{self._join_table_self_name}"
+                    + f"_{self._join_table_foreign_name}_unique"
+                ),
             ),
             keep_existing=True,
         )
